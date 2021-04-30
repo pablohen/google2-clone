7569d893da6533fdcb4de9e6d6000f49d4030a81
Pablo Henrique <pablo_hen@hotmail.com>
2021-04-30 12:10:25 -0300

    add Image from next.js

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,13 +1,13 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Avatar = ({ url, className }) => {
   return (
-    <img
-      loading="lazy"
+    <Image
       src={url}
       alt="profile pic"
-      width="460px"
-      height="460px"
+      width={48}
+      height={48}
       className={`rounded-full w-10 h-10 cursor-pointer transition duration-150 transform hover:scale-150 ${className}`}
     />
   );
