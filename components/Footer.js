507b71b0065db47e56daa7cc439d2542376f53e4
Pablo Henrique <pablo_hen@hotmail.com>
2021-04-30 11:20:29 -0300

    fixed low contrast text

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import { GlobeIcon } from '@heroicons/react/solid';
 
 const Footer = () => {
   return (
-    <footer className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 text-sm text-gray-500">
+    <footer className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 text-sm text-gray-700">
       <div className="px-8 py-3">
         <p>Brazil</p>
       </div>
